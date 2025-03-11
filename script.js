516a7bd27b10769e7370911137c1f0ca8d9e3983
Hector Acevedo <HEA092024@gmail.com>
2025-03-11 11:32:15 +0100

    added logic inside the function startGame

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ let sum = firstCard + secondCard
 let hasBlackJack = false 
 let isAlive = true 
 let message = ""
+
+function startGame () {
+    if (sum < 20) {
+        message = " Do you want to draw a new card?"
+    } else if ( sum === 21) {
+        message = " Wohoo, You've got Blackjack!"
+    }else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+}
+console.log(message)
